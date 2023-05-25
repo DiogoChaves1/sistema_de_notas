ac84f3961cc4436e84e75930914d47582c04048f
DiogoChaves1 <118543063+DiogoChaves1@users.noreply.github.com>
2023-05-25 10:24:15 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,35 @@
+public class Aluno {
+    private String nome;
+    private String resposta;
+    private double nota;
+    
+    public Aluno(String nome, String resposta){
+        this.nome=nome;
+        this.resposta=resposta;
+
+    }
+
+
+
+
+
+
+
+
+
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    public String getResposta(){
+        return resposta;
+    }
+
+    public void setResposta(String resposta){
+        this.resposta=resposta;
+    }
+}
